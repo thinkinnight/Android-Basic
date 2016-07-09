ac42f8d822d1df71b07218254b7360d71629f8ca
thinkinnight <thinkinnight@gmail.com>
2016-07-09 21:27:50 +0800

    add new activity

---
+++ 01_Activity/app/src/main/java/SecondActivity.java
@@ -0,0 +1,15 @@
+import android.app.Activity;
+import android.os.Bundle;
+
+import example.com.activitytest.R;
+
+/**
+ * Created by yaoqu on 7/9/2016.
+ */
+public class SecondActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_layout);
+    }
+}
