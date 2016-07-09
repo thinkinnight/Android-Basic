e780f0b6092dc11e0b6d818e90fe3f5e8f4b6b11
thinkinnight <thinkinnight@gmail.com>
2016-07-09 20:09:17 +0800

    没有出现action bar，很奇怪

--- 01_Activity/app/src/main/java/example/com/activitytest/FirstActivity.java
+++ 01_Activity/app/src/main/java/example/com/activitytest/FirstActivity.java
@@ -2,6 +2,7 @@ package example.com.activitytest;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
 
 /**
  * Created by yaoqu on 7/9/2016.
@@ -10,6 +11,7 @@ public class FirstActivity extends Activity{
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_ACTION_BAR);
         setContentView(R.layout.first_layout);
     }
 }
