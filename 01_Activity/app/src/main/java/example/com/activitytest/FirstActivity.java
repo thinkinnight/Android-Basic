37836e3473039625c838e67e462b44addc5c6dfd
thinkinnight <thinkinnight@gmail.com>
2016-07-09 20:18:35 +0800

    增加了Button和点击事件，但是标题栏还是显示不了，很奇怪的问题，不知道是为什么

--- 01_Activity/app/src/main/java/example/com/activitytest/FirstActivity.java
+++ 01_Activity/app/src/main/java/example/com/activitytest/FirstActivity.java
@@ -2,7 +2,10 @@ package example.com.activitytest;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
+import android.widget.Toast;
 
 /**
  * Created by yaoqu on 7/9/2016.
@@ -11,7 +14,15 @@ public class FirstActivity extends Activity{
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_ACTION_BAR);
         setContentView(R.layout.first_layout);
+
+        Button btn1 = (Button)findViewById(R.id.button_1);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(FirstActivity.this, "You clicked Button 1",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
