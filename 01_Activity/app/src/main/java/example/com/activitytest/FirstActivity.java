2b0346c5833ebe185811c4113bd8bd6c787c8cec
thinkinnight <thinkinnight@gmail.com>
2016-07-09 22:22:00 +0800

    隐式intent的使用，但是具体如何使用呢？

--- 01_Activity/app/src/main/java/example/com/activitytest/FirstActivity.java
+++ 01_Activity/app/src/main/java/example/com/activitytest/FirstActivity.java
@@ -23,7 +23,7 @@ public class FirstActivity extends Activity{
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(FirstActivity.this, SecondActivity.class);
+                Intent intent=new Intent("com.example.activitytest.ACTION_START");
                 startActivity(intent);
             }
         });
