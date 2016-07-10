5dfc1748732d8cfefe0e3363e57bf9050e3cbc6a
thinkinnight <thinkinnight@gmail.com>
2016-07-10 22:35:18 +0800

    intent return

--- 01_Activity/app/src/main/java/example/com/activitytest/FirstActivity.java
+++ 01_Activity/app/src/main/java/example/com/activitytest/FirstActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -26,10 +27,22 @@ public class FirstActivity extends Activity{
             public void onClick(View v) {
                 String data = "Hello second activity";
                 Intent intent=new Intent(FirstActivity.this, SecondActivity.class);
-                intent.putExtra("extra_data", data);
-                startActivity(intent);
+                startActivityForResult(intent, 1);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        switch (requestCode){
+            case 1:
+                if(resultCode==RESULT_OK){
+                    String returnedData = data.getStringExtra("data_return");
+                    Log.d("FirstActivity", returnedData);
+                }
+                break;
+            default:
+        }
     }
 
     @Override
--- 01_Activity/app/src/main/java/example/com/activitytest/SecondActivity.java
+++ 01_Activity/app/src/main/java/example/com/activitytest/SecondActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import example.com.activitytest.R;
 
@@ -15,8 +17,15 @@ public class SecondActivity extends Activity{
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.second_layout);
-        Intent intent = getIntent();
-        String data=intent.getStringExtra("extra_data");
-        Log.d("SecondActivity", data);
+        Button button2=(Button)findViewById(R.id.button_2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.putExtra("data_return", "Hello first activity");
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        });
     }
 }
